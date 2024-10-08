d7a7baac166b2c7b2b02f194dae4c34bb26c58c1
Md. Samim Akhter <Samim.Akhter@brainstation-23.com>
2024-10-08 14:47:01 +0600

    Entity and Repository created

---
+++ account-management/src/main/java/com/practice/accountmanagement/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.practice.accountmanagement.repository;
+
+import com.practice.accountmanagement.domain.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
