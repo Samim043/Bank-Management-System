78fdac603d33676ad021b8fc1acb2f0b1d4b34ec
Md. Samim Akhter <Samim.Akhter@brainstation-23.com>
2024-10-30 14:26:11 +0600

    card services

---
+++ card-management/src/test/java/com/practice/cardmanagement/CardManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.cardmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
