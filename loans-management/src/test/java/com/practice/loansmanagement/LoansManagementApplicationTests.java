cd8d1e49f5188c6f878e79b2503d9e10ae70f6bf
Md. Samim Akhter <Samim.Akhter@brainstation-23.com>
2024-11-06 12:33:59 +0600

    Loans Management

---
+++ loans-management/src/test/java/com/practice/loansmanagement/LoansManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.loansmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
