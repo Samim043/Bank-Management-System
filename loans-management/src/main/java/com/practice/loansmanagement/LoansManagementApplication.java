2a8f715b6613edf6b4efbedffb84f2ed9d27a6b3
Md. Samim Akhter <Samim.Akhter@brainstation-23.com>
2024-11-13 14:59:38 +0600

    refactored

--- loans-management/src/main/java/com/practice/loansmanagement/LoansManagementApplication.java
+++ loans-management/src/main/java/com/practice/loansmanagement/LoansManagementApplication.java
@@ -1,9 +1,39 @@
 package com.practice.loansmanagement;
 
+import com.practice.loansmanagement.domain.dto.LoansContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Samim",
+						email = "srsamim043@gmail.com",
+						url = "url"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "url"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Loans microservice REST API Documentation",
+				url = "url"
+		)
+)
 public class LoansManagementApplication {
 
 	public static void main(String[] args) {
